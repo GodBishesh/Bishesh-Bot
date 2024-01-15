93555cc97f7589b51d873d5ea38ef18b0970dd8f
Bishesh <134049028+GodBishesh@users.noreply.github.com>
2024-01-15 06:55:52 +0545

    Update event.js

--- handlers/event.js
+++ handlers/event.js
@@ -1,1 +1,8 @@
-
+ module.exports = ({ api, event, config, userInfo }) => {
+  if (event.logMessageType == "log:subscribe") {
+    require("../handlers/events/join.js")({ api, event, config });
+  } else if (event.logMessageType == "log:unsubscribe") {
+    require("../handlers/events/left.js")({ api, event, config });
+  } else {
+  }
+};
