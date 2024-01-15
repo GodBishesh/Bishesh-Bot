6e944745d66201d0cedba6cba91fcd13ebdb13b5
Bishesh <134049028+GodBishesh@users.noreply.github.com>
2024-01-15 06:58:28 +0545

    Update message.js

--- handlers/message.js
+++ handlers/message.js
@@ -1,1 +1,138 @@
+module.exports = async ({ api, event, config, userInfo }) => {
+  let input = event.body;
 
+  const sim = [
+    {
+        trigger: "hello", 
+        responses: [
+            "Hello", 
+            "Hallooo", 
+            "Zup Maderpaker"
+        ]
+    },
+    {
+        trigger: "hora", 
+        responses: [
+            "Haina hola", 
+            "Ambo sachhi ho ?", 
+            "haina ni tmi jhukkiyeau "
+        ]
+    },
+    {
+        trigger: "xkka", 
+        responses: [
+            "tai hos sala xakka", 
+            "xakka hos aije mero lado khelaidey", 
+            "Tero bau hola xakka"
+        ]
+    },
+    {
+        trigger: "Hi", 
+        responses: [
+            "Hi sir namaste", 
+            "muji namskar van", 
+            "hi k xa sathi"
+        ]
+    },
+    {
+        trigger: "Machikne", 
+        responses: [
+            "chup muji", 
+            "Chikne badi vako", 
+            "Oi muji randi"
+        ]
+    },
+    {
+        trigger: "muji", 
+        responses: [
+            "teri ama muji", 
+            "tai muji", 
+            " fuck you"
+        ]
+    }, 
+];
+
+  function simsimi(prompt, array) {
+      const lowerPrompt = prompt.toLowerCase();
+
+      const foundItem = array.find(item => lowerPrompt.includes(item.trigger));
+
+      if (foundItem) {
+          const randomIndex = Math.floor(Math.random() * foundItem.responses.length);
+          return foundItem.responses[randomIndex];
+      }
+
+      return "";
+  }
+
+
+  let cID = api.getCurrentUserID();
+  let currentUserInfo = await api.getUserInfo(cID);
+  currentUserInfo = currentUserInfo[cID];
+  let aiPrefix = currentUserInfo.firstName.split(" ");
+  const { name, prefix } = config;
+  //Prefix based commands
+  if (input.startsWith(`${prefix}`)) {
+    let cmd = input.substring(1);
+    cmd = cmd.split(" ");
+    try {
+      if (cmd[0].length == 0) {
+        return api.sendMessage(
+          {
+            body: "Yess " + userInfo.firstName + "?, that's my f*cking prefix.",
+          },
+          event.threadID,
+          event.messageID
+        );
+      } else {
+        let runIt = require(`../utilities/commands/${cmd[0]}`);
+        runIt({ api, event, config, userInfo });
+      }
+    } catch (err) {
+      //If the file not foundor something error.
+      if (err.code == "MODULE_NOT_FOUND") {
+        api.sendMessage(
+          `Command '${cmd[0]}' is not f*cking found on command list.`,
+          event.threadID,
+          event.messageID
+        );
+      } else {
+        console.log(err);
+        api.sendMessage(`Error: ${err}`, event.threadID, event.messageID);
+      }
+    }
+  }
+  //Personalized AI based on user name!
+  else if (input.startsWith(aiPrefix[0])) {
+    let data = input.split(" ");
+    if(data.length < 2) {
+      api.sendMessage("What?", event.threadID, event.messageID);
+    } else {
+      try {
+        data.shift()
+        let ai = require("../aimodels/default");
+        ai({ text: data.join(" "), userInfo, aiPrefix }, (res) => {
+          api.sendMessage(res, event.threadID, event.messageID)
+        })
+      } catch (err) {
+        api.sendMessage(`Error: ${err}`, event.threadID, event.messageID)
+      }
+    }
+  }
+  //Code block of no prefix!
+  else {
+    try {
+      let cmd = input.split(" ");
+      let runIt = require(`../utilities/commands/noprefix/${cmd[0]}`);
+      runIt({ api, event, config, userInfo });
+    } catch (err) {
+      if(err.code == "MODULE_NOT_FOUND") {
+          let inp = input.toLowerCase();
+          const response = simsimi(inp, sim);
+          api.sendMessage(response, event.threadID, event.messageID)
+      } else {
+          return;
+      }
+    }
+  }
+};
